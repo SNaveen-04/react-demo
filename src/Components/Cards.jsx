1acd40fb9a66f16b092dd58c985d0b6dfab7e49d
Naveen_S <naveen80567@gmail.com>
2024-01-24 16:01:05 +0530

    nothing

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -2,7 +2,18 @@ import { Grid } from "semantic-ui-react"
 import users from '../Data.json'
 import UserCard from "./UserCard"
 import Header from "./Header"
-import styles from '
+import styles from '../styles/app.module.css'
+
+const Test = () => {
+  return (
+    <div>
+    <Header data='Card'/>
+    <div className={styles.CardContainer}>
+    <Grid  columns={4} textAlign="center" verticalAlign="middle">
+      {users.map(({id,...rest})=><UserCard key={id} user={rest}/>)}
+    </Grid>
+    </div>
+    </div>
   )
 }
 
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,21 +1,7 @@
 const Form = () => {
   return (
     <form></form>
-        <form></form>
-form>
   )
 }
 
-export default Form../styles/app.module.css'
-
-const Test = () => {
-  return (
-    <div>
-    <form></form>
-    les.CardContainer}>
-    <Grid  columns={4} textAlign="center" verticalAlign="middle">
-      {users.map(({id,...rest})=><UserCard key={id} user={rest}/>)}
-    </Grid>
-    </div>
-    </div>
-    </div>
+export default Form
